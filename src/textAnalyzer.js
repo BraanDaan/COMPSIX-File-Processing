9eb80e01e38b299ee356da62007b96f5607ded88
Brandon M. <branmit12@gmail.com>
2025-11-01 16:36:03 -0700

    First commit, First edit

---
+++ src/textAnalyzer.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+
+
+function wordCount(fileContents) {
+    let words = 0;
+    return;
+}
+
+const quotesContent = fs.readFileSync('./data/quotes.txt');
+
+// Count total number of words
+// Find the Longest word
+// Count how many lines the file has
+
+console.log("")
